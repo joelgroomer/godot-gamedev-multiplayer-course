6d95a977bec09a6c65badd862f4608318958efb5
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-12 14:26:57 -0700

    Player movement animations

--- player/player.gd
+++ player/player.gd
@@ -16,10 +16,25 @@ func _physics_process(delta):
 	velocity.x = h_input * move_speed
 	velocity.y += gravity
 	
-	if Input.is_action_just_pressed("jump") && is_on_floor():
+	var is_falling = velocity.y > 0.0 && !is_on_floor()
+	var is_jumping = Input.is_action_just_pressed("jump") && is_on_floor()
+	var is_jump_cancelled = Input.is_action_just_released("jump") && velocity.y < 0.0
+	var is_idle = is_on_floor() && is_zero_approx(velocity.x)
+	var is_walking = is_on_floor() && !is_zero_approx(velocity.x)
+	
+	if is_jumping:
 		velocity.y = -jump_strength
 		
 	move_and_slide()
+	
+	if is_jumping:
+		player_sprite.play("jump_start")
+	elif is_walking:
+		player_sprite.play("walk")
+	elif is_falling:
+		player_sprite.play("fall")
+	elif is_idle:
+		player_sprite.play("idle")
 	
 	if not is_zero_approx(h_input):
 		if h_input < 0:
