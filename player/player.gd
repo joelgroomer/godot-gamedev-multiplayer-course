cd38b07250526d785e80de1571e1d3ee020d394b
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-12 14:57:39 -0700

    Attach camera to Player

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var player_camera: PackedScene
+@export var camera_height = -132
 @export var player_sprite: AnimatedSprite2D
 @export var move_speed = 300
 @export var gravity = 30
@@ -9,6 +11,15 @@ extends CharacterBody2D
 @onready var initial_sprite_scale = player_sprite.scale
 
 var jump_count = 0
+var camera_instance
+
+func _ready():
+	camera_instance = player_camera.instantiate()
+	camera_instance.global_position.y = camera_height
+	get_tree().current_scene.add_child.call_deferred(camera_instance)
+
+func _process(_delta):
+	camera_instance.global_position.x = global_position.x
 
 func _physics_process(_delta):
 	var h_input = (
