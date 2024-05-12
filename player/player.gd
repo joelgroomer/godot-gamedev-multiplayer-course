a4b99c2492ae55c87520a57ff350bf3af961d5f5
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-12 14:16:51 -0700

    Keep player facing proper direction after movement

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+@export var player_sprite: AnimatedSprite2D
 @export var move_speed = 300
 @export var gravity = 30
 @export var jump_strength = 600
 
+@onready var initial_sprite_scale = player_sprite.scale
 
 func _physics_process(delta):
 	var h_input = (
@@ -18,3 +20,9 @@ func _physics_process(delta):
 		velocity.y = -jump_strength
 		
 	move_and_slide()
+	
+	if not is_zero_approx(h_input):
+		if h_input < 0:
+			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
+		else:
+			player_sprite.scale = initial_sprite_scale
