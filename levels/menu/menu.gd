0412c438af5753570d96598d3a6a10b709eb6ff7
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-12 22:04:27 -0700

    Differentiate host/client UI, allow host to start game

--- levels/menu/menu.gd
+++ levels/menu/menu.gd
@@ -1,7 +1,12 @@
 extends Node
 
+@export var ui: Control
+@export var level_container: Node
+@export var level_scene: PackedScene
 @export var ip_line_edit: LineEdit
 @export var status_label: Label
+@export var not_connected_hbox: HBoxContainer
+@export var host_hbox: HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,18 +14,34 @@ func _ready():
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
 func _on_host_button_pressed():
+	not_connected_hbox.hide()
+	host_hbox.show()
 	Lobby.create_game()
 	status_label.text = "Waiting for players..."
 
 func _on_join_button_pressed():
+	not_connected_hbox.hide()
 	Lobby.join_game(ip_line_edit.text)
 	status_label.text = "Connecting..."
 
 func _on_start_button_pressed():
-	pass # Replace with function body.
+	change_level.call_deferred(level_scene)
+	hide_menu.rpc()
 
 func _on_connection_failed():
 	status_label.text = "Failed to connect"
+	not_connected_hbox.show()
 
 func _on_connected_to_server():
 	status_label.text = "Connected!"
+
+func change_level(scene):
+	for c in level_container.get_children():
+		level_container.remove_child(c)
+		c.queue_free()
+	
+	level_container.add_child(scene.instantiate())
+
+@rpc("call_local", "authority", "reliable")
+func hide_menu():
+	ui.hide()
