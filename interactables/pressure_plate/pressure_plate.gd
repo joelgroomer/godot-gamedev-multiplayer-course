b54ad8078826531d9652435e8978b9ed4d82e0cc
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-13 20:21:20 -0700

    Add PressurePlate

---
+++ interactables/pressure_plate/pressure_plate.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@export var is_down = false
+@export var plate_up: Sprite2D
+@export var plate_down: Sprite2D
+
+var bodies_on_plate = 0
+
+func _on_area_2d_body_entered(_body):
+	if not multiplayer.is_server():
+		return
+	bodies_on_plate += 1
+	set_plate_state()
+
+func _on_area_2d_body_exited(_body):
+	if not multiplayer.is_server():
+		return
+	bodies_on_plate -= 1
+	set_plate_state()
+
+func set_plate_state():
+	is_down = bodies_on_plate >= 1
+	set_plate_sprite()
+
+func set_plate_sprite():
+	plate_down.visible = is_down
+	plate_up.visible = !is_down
+
+func _on_multiplayer_synchronizer_delta_synchronized():
+	set_plate_sprite()
